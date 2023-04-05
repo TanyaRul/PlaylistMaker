546fcf54095530e9c851d4111f4894f4415ea9b4
TanyaRul <t.rul26@yandex.ru>
2023-04-05 17:19:06 +0300

    Спринт 9. Домашнее задание

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.content.Intent.*
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,9 +14,42 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<ImageView>(R.id.backToMainActivity)
+        val shareApp = findViewById<TextView>(R.id.share)
+        val support = findViewById<TextView>(R.id.support)
+        val termsOfUse = findViewById<TextView>(R.id.terms_of_use)
+
 
         backButton.setOnClickListener {
             finish()
         }
+
+        shareApp.setOnClickListener {
+            val urlCourse = getString(R.string.url_course)
+            val shareIntent = Intent(ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(EXTRA_TEXT, urlCourse)
+            startActivity(shareIntent)
+        }
+
+        support.setOnClickListener {
+            val email = getString(R.string.email_address)
+            val subject = getString(R.string.email_subject)
+            val message = getString(R.string.email_message)
+            val supportIntent = Intent(ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(EXTRA_EMAIL, arrayOf(email))
+            supportIntent.putExtra(EXTRA_SUBJECT, subject)
+            supportIntent.putExtra(EXTRA_TEXT, message)
+            startActivity(supportIntent)
+        }
+
+        termsOfUse.setOnClickListener {
+            val urlOffer = getString(R.string.url_offer)
+            val termsOfUseIntent = Intent(ACTION_VIEW)
+            termsOfUseIntent.data = Uri.parse(urlOffer)
+            startActivity(termsOfUseIntent)
+        }
+
+
     }
 }
